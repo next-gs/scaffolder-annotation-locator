cee37ad831b6d08a8078d7e7c0682a53659cc862
Michael Barton <mail@michaelbarton.me.uk>
2011-03-25 17:17:08 -0400

    Temporarily move helpers into spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,6 @@ $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'tempfile'
 
 require 'rspec'
-require 'scaffolder/test/helpers'
 require 'scaffolder/annotation_locator'
 
 # Requires supporting files with custom matchers and macros, etc,
@@ -12,5 +11,33 @@ require 'scaffolder/annotation_locator'
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
-  include Scaffolder::Test::Helpers
+
+  def write_sequence_file(entries,file = Tempfile.new("sequence").path)
+    File.open(file,'w') do |tmp|
+      make_sequence(entries).each do |entry|
+        seq = Bio::Sequence.new(entry[:sequence])
+        tmp.print(seq.output(:fasta,:header => entry[:name]))
+      end
+    end
+    file
+  end
+
+  def write_scaffold_file(entries,file = Tempfile.new("scaffold").path)
+    File.open(file,'w'){|tmp| tmp.print(YAML.dump(make_scaffold(entries)))}
+    file
+  end
+
+  def make_scaffold(entries)
+    entries.map do |entry|
+      {'sequence' => {'source' => (entry['name'] || entry[:name]) }}
+    end
+  end
+
+  def make_sequence(entries)
+    entries.map do |entry|
+      {:name => (entry['name'] || entry[:name]),
+        :sequence => (entry['nucleotides'] || entry[:nucleotides]) }
+    end.flatten
+  end
+
 end
