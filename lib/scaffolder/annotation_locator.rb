c46d408bdd5ae6df64f0577850ebcfd8132de358
Michael Barton <mail@michaelbarton.me.uk>
2011-03-25 23:41:49 -0400

    Passing specs for relocating a simple contig

--- lib/scaffolder/annotation_locator.rb
+++ lib/scaffolder/annotation_locator.rb
@@ -5,10 +5,28 @@ require 'bio'
 class Scaffolder::AnnotationLocator < DelegateClass(Array)
 
   def initialize(scaffold_file,sequence_file,gff_file)
+    @scaffold_file = scaffold_file
+    @sequence_file = sequence_file
+
     super(Bio::GFF::GFF3.new(File.read(gff_file)).records.map! do |record|
+
+      sequence = sequences[record.seqname]
+      if sequence.start > 1
+        record.start -= sequence.start - 1
+        record.end   -= sequence.start - 1
+      end
+
       record.seqname = "scaffold"
       record
     end)
+  end
+
+  def sequences
+    scaffold = Scaffolder.new(YAML.load(File.read(@scaffold_file)),@sequence_file)
+    scaffold.inject(Hash.new) do |hash,entry|
+      hash[entry.source] = entry if entry.entry_type == :sequence
+      hash
+    end
   end
 
 end
--- spec/scaffolder/annotation_locator_spec.rb
+++ spec/scaffolder/annotation_locator_spec.rb
@@ -5,7 +5,7 @@ describe Scaffolder::AnnotationLocator do
   describe "relocating a single annotation on a single contig" do
 
     before do
-      entries = [{:name => 'seq1', :nucleotides => 'ATGC'}]
+      entries = [{:name => 'contig1', :nucleotides => 'ATGC'}]
 
       @record = { :seqname => 'contig1',
         :start => 4, :end => 6, :strand => '+',:phase => 1}
@@ -43,7 +43,7 @@ describe Scaffolder::AnnotationLocator do
   describe "relocating a single annotation on a single contig" do
 
     before do
-      entries = [{:name => 'seq1', :nucleotides => 'ATGC', :start => 4}]
+      entries = [{:name => 'contig1', :nucleotides => 'ATGC', :start => 4}]
 
       @record = { :seqname => 'contig1',
         :start => 4, :end => 6, :strand => '+',:phase => 1}
@@ -74,6 +74,26 @@ describe Scaffolder::AnnotationLocator do
 
     it "should have same phase" do
       subject.phase.should == @record[:phase]
+    end
+
+  end
+
+  describe "the sequences hash" do
+
+    before do
+      @name = 'something'
+      entries = [{:name => @name, :nucleotides => 'ATGC'}]
+      @gff3_file = generate_gff3_file([])
+      @scaffold_file = write_scaffold_file(entries)
+      @sequence_file = write_sequence_file(entries)
+    end
+
+    subject do
+      described_class.new(@scaffold_file,@sequence_file,@gff3_file)
+    end
+
+    it "should contain the expected sequence as a hash key" do
+      subject.sequences.keys.should == [@name]
     end
 
   end
