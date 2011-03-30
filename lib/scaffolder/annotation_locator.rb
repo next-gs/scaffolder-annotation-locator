347660319fb7bff3c84f27bceb4cb51a074e8a27
Michael Barton <mail@michaelbarton.me.uk>
2011-03-30 10:07:27 -0400

    Implement returning ordered set of gff3 annotations

--- lib/scaffolder/annotation_locator.rb
+++ lib/scaffolder/annotation_locator.rb
@@ -8,31 +8,44 @@ class Scaffolder::AnnotationLocator < DelegateClass(Array)
     @scaffold_file = scaffold_file
     @sequence_file = sequence_file
 
+    gff3 = Bio::GFF::GFF3.new(File.read(gff_file)).records
+    records_by_sequence = gff3.inject(Hash.new) do |hash,record|
+      hash[record.seqname] ||= Array.new
+      hash[record.seqname] << record
+      hash
+    end
+
+    updated_records = Array.new
     running_length = 0
-    super(Bio::GFF::GFF3.new(File.read(gff_file)).records.map! do |record|
+    sequences.each do |name,scaffold_entry|
+      records = records_by_sequence[name]
+      next if records.nil?
+      records.each do |record|
 
-      scaffold_entry = sequences[record.seqname]
-      if scaffold_entry.start > 1
-        record.start -= scaffold_entry.start - 1
-        record.end   -= scaffold_entry.start - 1
+        if scaffold_entry.start > 1
+          record.start -= scaffold_entry.start - 1
+          record.end   -= scaffold_entry.start - 1
+        end
+
+        if scaffold_entry.reverse
+          record.end   = scaffold_entry.sequence.length - (record.end - 1)
+          record.start = scaffold_entry.sequence.length - (record.start - 1)
+
+          record.end, record.start = record.start, record.end
+          record.strand = self.class.flip_strand(record.strand)
+        end
+
+        record.start += running_length
+        record.end   += running_length
+
+        running_length += scaffold_entry.sequence.length
+
+        record.seqname = "scaffold"
+        updated_records << record
       end
+    end
 
-      if scaffold_entry.reverse
-        record.end   = scaffold_entry.sequence.length - (record.end - 1)
-        record.start = scaffold_entry.sequence.length - (record.start - 1)
-
-        record.end, record.start = record.start, record.end
-        record.strand = self.class.flip_strand(record.strand)
-      end
-
-      record.start += running_length
-      record.end   += running_length
-
-      running_length += scaffold_entry.sequence.length
-
-      record.seqname = "scaffold"
-      record
-    end)
+    super updated_records
   end
 
   def sequences
--- spec/scaffolder/annotation_locator_spec.rb
+++ spec/scaffolder/annotation_locator_spec.rb
@@ -2,6 +2,28 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
 
 describe Scaffolder::AnnotationLocator do
 
+  describe "relocating a an empty gff file" do
+
+    before(:all) do
+      entries = [{:name => 'contig1', :nucleotides => 'ATGC'}]
+
+      @gff3_file = generate_gff3_file([])
+      @scaffold_file = write_scaffold_file(entries)
+      @sequence_file = write_sequence_file(entries)
+
+      @annotations = described_class.new(@scaffold_file, @sequence_file, @gff3_file)
+    end
+
+    subject do
+      @annotations
+    end
+
+    it "should return an empty annotation array" do
+      subject.should be_empty
+    end
+
+  end
+
   describe "relocating a single annotation on a single contig" do
 
     before(:all) do
@@ -140,6 +162,40 @@ describe Scaffolder::AnnotationLocator do
     # First contig length: 6
     it{ should set_the_attribute(:start   => 10).only_for_the(:second) }
     it{ should set_the_attribute(:end     => 12).only_for_the(:second) }
+
+  end
+
+  describe "relocating two unordered annotations on two contigs" do
+
+    before(:all) do
+      @sequences = [{:name => 'c1', :nucleotides => 'AAATTT'},
+                    {:name => 'c2', :nucleotides => 'AAATTT'}]
+
+      one = {:seqname => 'c1', :start => 1, :end => 3, :strand => '-',:phase => 1}
+      two = {:seqname => 'c2', :start => 4, :end => 6, :strand => '+',:phase => 1}
+      @entries = [two,one]
+
+      @gff3_file = generate_gff3_file(@entries)
+      @scaffold_file = write_scaffold_file(@sequences)
+      @sequence_file = write_sequence_file(@sequences)
+      @annotations = described_class.new(@scaffold_file, @sequence_file, @gff3_file)
+    end
+
+    subject do
+      @annotations
+    end
+
+    it{ should set_the_attribute(:seqname => 'scaffold') }
+    it{ should set_the_attribute(:phase   => 1) }
+
+    it{ should set_the_attribute(:start   => 1).only_for_the(:first) }
+    it{ should set_the_attribute(:end     => 3).only_for_the(:first) }
+    it{ should set_the_attribute(:strand  => '-').only_for_the(:first) }
+
+    # First contig length: 6
+    it{ should set_the_attribute(:start   => 10).only_for_the(:second) }
+    it{ should set_the_attribute(:end     => 12).only_for_the(:second) }
+    it{ should set_the_attribute(:strand  => '+').only_for_the(:second) }
 
   end
 
