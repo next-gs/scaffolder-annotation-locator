44ce194f6d07ed43488c700a10c5c4edb22d5bb1
Michael Barton <mail@michaelbarton.me.uk>
2011-04-01 12:23:17 -0400

    Add failure message for attribute matcher

--- spec/support/gff_attribute_matcher.rb
+++ spec/support/gff_attribute_matcher.rb
@@ -5,17 +5,30 @@ RSpec::Matchers.define :set_the_attribute do |expected|
     @attribute = expected.keys.first
     @value     = expected.values.first
     if @ordinal
-      annotations[ORDINALS.index(@ordinal)].send(@attribute) == @value
+      @actual = annotations[ORDINALS.index(@ordinal)].send(@attribute)
+       @actual == @value
     else
       annotations.all?{|a| a.send(@attribute) == @value }
     end
   end
+
   chain :only_for_the do |ordinal|
     @ordinal = ordinal
   end
+
   description do
     string = "set the annotation #{@attribute} attribute to \"#{@value}\""
     string << " for the #{@ordinal} annotation" if @ordinal
     string
   end
+
+  failure_message_for_should do |annotations|
+    message = "expected \"#{@attribute}\" to be \"#{@value}\" "
+    message + if @ordinal
+      "but was \"#{actual}\" for the #{@ordinal} annotation"
+    else
+      "for all annotations"
+    end
+  end
+
 end
